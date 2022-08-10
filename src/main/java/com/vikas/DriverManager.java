de79fc4013634fa6dd08b690382cee68bba36b89
vikas-thange-twc <vikas.thange@gmail.com>
2022-08-10 16:40:43 +0000

    added questions

--- src/main/java/com/vikas/DriverManager.java
+++ src/main/java/com/vikas/DriverManager.java
@@ -15,7 +15,8 @@ public class DriverManager {
     }
     public void initDriver(String url, String ltUserName, String ltToken, Map<String,String> caps){
 
-
+        // Question: Ensure that network logs, video recording, screenshots, & console
+        // logs are enabled through the desired capabilities generator.
 
         // Question: TimeOut of the test duration should be set to 20 seconds (for both the test scenarios). Parallelism should be at the Class Level (i.e., both the tests should be executing in parallel on LambdaTest).
     }
--- src/test/java/com/test/AssignmentTest.java
+++ src/test/java/com/test/AssignmentTest.java
@@ -5,17 +5,15 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AssignmentTest{
-
-    public static void main(String[] args) {
-        System.out.println("Hi");
-        
-    }
-    @Test(dataProvider = "OS_Browsers_Data_Factory")
+    // Question: Keep it simple
+    // TimeOut of the test duration should be set to 20 seconds (for both the test scenarios). 
+    @Test(dataProvider = "OS_Browsers_Data_Factory",timeOut = 20000)
     public void test1(String browserName,String browserVersion,String osName){
+        
         Assert.assertTrue(true);
     }
-    
-    @DataProvider(name="OS_Browsers_Data_Factory")
+    // Question: You must run the test on LambdaTest Cloud Selenium Grid in parallel and mention the final Test ID(s) while submitting.
+    @DataProvider(name="OS_Browsers_Data_Factory",parallel = true)
     public Object[][] getTestEnvironmentParams(){
 
         // Question: If you are using Java and TestNG, then pass the browser and OS combinations to the test scenario(s) from testng.xml (for Java). You could also hard code the browser and OS combination in the implementation.
